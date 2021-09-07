7c4f053b96717c87bfa150a86b39c60f5dfe28a0
EljovenAleman <diegobettendorf@gmail.com>
2021-09-07 19:23:28 -0300

    Added new canvases to visualize main and addMovie views

---
+++ BettReviews/Assets/Scripts/Delivery/SelectActionView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectActionView : MonoBehaviour
+{
+    [SerializeField] Button goToAddMovieViewButton;
+    [SerializeField] Button goToAddReviewViewButton;
+
+    [SerializeField] GameObject addMovieView;
+    [SerializeField] GameObject addReviewView;
+
+
+
+    void Start()
+    {
+        goToAddMovieViewButton.onClick.AddListener(GoToAddMovieView);
+        goToAddReviewViewButton.onClick.AddListener(GoToAddReviewView);
+    }
+    
+    void GoToAddMovieView()
+    {
+        gameObject.SetActive(false);
+        addMovieView.SetActive(true);
+    }
+
+    void GoToAddReviewView()
+    {
+        gameObject.SetActive(false);
+        addReviewView.SetActive(true);
+    }
+
+}
